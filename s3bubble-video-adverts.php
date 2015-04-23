6726e195f5ac3df72a87b72a08ad6dc90c8fb24d
s3bubble <s3bubble@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-23 12:09:04 +0000

    Brand New Player Design And Analytics Media Tracking Within The New Admin
    git-svn-id: https://plugins.svn.wordpress.org/s3bubble-amazon-s3-html-5-video-with-adverts/trunk@1143211 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- s3bubble-video-adverts.php
+++ s3bubble-video-adverts.php
@@ -3,7 +3,7 @@
 Plugin Name: S3Bubble Cloud Video With Adverts & Analytics
 Plugin URI: https://www.s3bubble.com/
 Description: S3Bubble offers simple, secure media streaming from Amazon S3 to WordPress and adding your very own adverts with analytics. In just 4 simple steps. 
-Version: 0.6
+Version: 0.7
 Author: S3Bubble
 Author URI: https://s3bubble.com/
 License: GPL2
