ebbed401dd656d38c4ba2d4513bd24674039a6a6
s3bubble <s3bubble@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-07 19:03:30 +0000

    File fix
    git-svn-id: https://plugins.svn.wordpress.org/s3bubble-amazon-s3-html-5-video-with-adverts/trunk@1194322 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- assets/plugins/ultimate/content/downloader.php
+++
@@ -1,6 +0,0 @@
-<?php 
-   header("Content-Type: application/octet-stream");
-   header("Content-Disposition: attachment; filename=". $_GET['name']);
-   $path = urldecode($_GET['path']);
-   if(isset($path))readfile($path);
-?>
